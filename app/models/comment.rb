5226c995fa961229ae539dc8424fc591ca09ce3f
hot-and-cool <hisato0217take@gmail.com>
2020-01-30 14:04:47 +0900

    ユーザーヘッダー編集

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :from_user, class_name: "User"
   
   validates :text, presence: false
-  validates :rate, numericality: {less_than_or_equal_to: 5,greater_than_or_equal_to: 1}, presence: true
+  validates :rate, numericality: {less_than_or_equal_to: 5,greater_than_or_equal_to: 0.5}, presence: true
 
   
 
